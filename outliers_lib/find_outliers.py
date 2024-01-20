47ebfebed8936803cc581fdb43974b88929094af
AlexVasilev25 <148490379+AlexVasilev25@users.noreply.github.com>
2024-01-20 14:52:17 +0300

    added function find_outliers_quantile

--- outliers_lib/find_outliers.py
+++ outliers_lib/find_outliers.py
@@ -58,3 +58,26 @@ def find_outliers_z_score(data, feature, left=3, right=3, log_scale=False):
     outliers = data[(x < lower_bound) | (x > upper_bound)]
     cleaned = data[(x > lower_bound) & (x < upper_bound)]
     return outliers, cleaned
+
+def find_outliers_quantile(data, feature, left=0.01, right=0.99):
+    """
+    Находит выбросы в данных, наблюдения которые не попадают в интервал
+    обозначенный квантилями, считаются выбросами. Функция будет 
+    отсикать 1 процент наблюдений слева и 1 процент справа.
+    Args:
+        data (pandas.DataFrame): набор данных
+        feature (str): имя признака, на основе которого происходит поиск выбросов 
+        left (float, optional): граница с левой стороны распределения. По умолчанию 0.01.
+        right (float, optional): граница с правой стороны распределения. Defaults to 0.99.
+
+    Returns:
+        pandas.DataFrame : наблюдения, попавшие в разряд выбросов
+        pandas.DataFrame : очищенные данные, из которых исключены выбросы
+    """
+    x = data[feature]
+    lower_bound = x.quantile(left)
+    upper_bound = x.quantile(right)
+    outliers = data[(x < lower_bound) | (x > upper_bound)]
+    cleaned = data[(x > lower_bound) & (x < upper_bound)]
+    return outliers, cleaned
+
